259b5230dedf4ed8ab9d9fdab4df12eec0b127cf
Tommaso Batori <tommasobatori@gmail.com>
2025-12-07 18:14:52 +0100

    Solve Q7b

--- Day 7/problem_7.cpp
+++ Day 7/problem_7.cpp
@@ -6,7 +6,7 @@
 #include <algorithm>
 #include <cctype>
 
-const std::string file = "input_p7_test.txt";
+const std::string file = "input_p7.txt";
 
 int main(){
     std::ifstream f(file);
@@ -15,10 +15,10 @@ int main(){
     std::vector<std::vector<int>> beam_path;
     std::vector<std::vector<int>> obstacles;
     std::vector<std::vector<int>> beam_idxs; 
-    std::vector<std::vector<int>> timelines; 
+    std::vector<std::vector<unsigned long>> timelines; 
     int r = 0;
     int total1 = 0;
-    int total2 = 0;
+    unsigned long total2 = 0;
 
     while(getline(f, s)){
         std::vector<int> row(s.size(), 0);
@@ -48,13 +48,12 @@ int main(){
     for(int i=1; i<obstacles.size();++i){
         std::vector<int> obs_row = obstacles[i];
         std::vector<int> new_beams;
-        std::vector<int> new_timelines;
+        std::vector<unsigned long> new_timelines;
         std::cout << i << "/" << obstacles.size()<<std::endl;
         int k = 0;
         for(auto idx : beam_idxs[i-1]){
             if (obs_row[idx] == 1){
                 total1++;
-                // if ((obs_row[idx-1]==0)&&(std::find(new_beams.begin(), new_beams.end(), idx-1) == new_beams.end())){
                 if ((obs_row[idx-1]==0)){
                     auto it = std::find(new_beams.begin(), new_beams.end(), idx-1);
                     if (it == new_beams.end()){
@@ -62,26 +61,38 @@ int main(){
                         new_timelines.push_back(timelines[i-1][k]);
                     } else {
                         int beam_idx_int = std::distance(new_beams.begin(), it);
-                        new_timelines[beam_idx_int] += timelines[idx-1][k];
+                        new_timelines[beam_idx_int] += timelines[i-1][k];
                     }
-                    // timelines.push_back
                 }
-                // if ((obs_row[idx+1]==0)&&(std::find(new_beams.begin(), new_beams.end(), idx+1) == new_beams.end())){
                 if ((obs_row[idx+1]==0)){
 
                     if (std::find(new_beams.begin(), new_beams.end(), idx+1) == new_beams.end()){
                         new_beams.push_back(idx+1);
+                        new_timelines.push_back(timelines[i-1][k]);
                     }
                 }
             } else {
-                if(std::find(new_beams.begin(), new_beams.end(), idx) == new_beams.end()){
+                auto it = std::find(new_beams.begin(), new_beams.end(), idx);
+                if (it == new_beams.end()){
                     new_beams.push_back(idx);
+                    new_timelines.push_back(timelines[i-1][k]);
+                } else {
+                    int beam_idx_int = std::distance(new_beams.begin(), it);
+                    new_timelines[beam_idx_int] += timelines[i-1][k];
                 }
+
+                // if(it == new_beams.end()){
+                //     new_beams.push_back(idx);
+                // }
             }
             k++;
         }
         timelines.push_back(new_timelines);
         beam_idxs.push_back(new_beams);
+    }
+
+    for(int i=0; i<timelines[obstacles.size()-1].size(); i++){
+        total2 += timelines[obstacles.size()-1][i];
     }
 
     return 1;
