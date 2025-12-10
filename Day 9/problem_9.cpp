97b2e59ea530ce04194df7e545226215b826a6e1
Tommaso Batori <tommasobatori@gmail.com>
2025-12-10 01:00:49 +0100

    Solve Q9a

---
+++ Day 9/problem_9.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+#include <vector>
+#include <array>
+#include <regex>
+#include <algorithm>
+#include <queue>
+#include <tuple>
+
+const std::string file = "input_p9.txt";
+const std::regex dc(R"(\d+)");
+const char del = ',';
+
+int main(){
+    std::ifstream f(file);
+    std::string s;
+
+    std::vector<std::array<unsigned int, 2>> data;
+
+    while(getline(f,s)){
+        std::array<unsigned int, 2> p;
+        int i = 0;
+        int k = 0;
+        std::sregex_iterator it(s.begin(), s.end(), dc), end;
+        for (it; it!=end;++it){
+            std::string m = (*it)[0].str();
+            p[k] = stoi(m);
+            k++;
+        }
+        data.push_back(p);
+    }
+
+    unsigned long long max = 0;
+    int n = data.size();
+    unsigned long long areas[n][n];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            unsigned long long a = (unsigned long long)abs(data[i][0] - data[j][0] + 1)* (unsigned long long)abs(data[i][1] - data[j][1] + 1);
+            if (a>max) max = a;
+            areas[i][j] = a;
+        }
+    }
+
+    std::cout << "Result (part 1): " << max << std::endl;
+
+    return 0;
+}
